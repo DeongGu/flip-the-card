6b64f4f45a42c13fb6cb84c803e9a869867c40a8
SeongWoo Shin <ssw4510@gmail.com>
2023-11-07 23:19:47 +0900

    fix css flip motion (버벅거리는 현상 수정)

--- src/Card.js
+++ src/Card.js
@@ -7,9 +7,13 @@ export default class Card {
     const cardWrapper = document.createElement("div");
     cardWrapper.setAttribute("class", "cardWrapper");
 
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    cardWrapper.appendChild(card);
+
     const front = document.createElement("div");
     front.setAttribute("class", "card_front");
-    cardWrapper.appendChild(front);
+    card.appendChild(front);
 
     const img = document.createElement("img");
     img.setAttribute("class", "card_image");
@@ -19,7 +23,7 @@ export default class Card {
     const back = document.createElement("div");
     back.setAttribute("class", "card_back");
     back.innerText = "뒷면";
-    cardWrapper.appendChild(back);
+    card.appendChild(back);
 
     this.$target.appendChild(cardWrapper);
   }
